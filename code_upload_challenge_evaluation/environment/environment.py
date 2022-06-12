46a648c15ff8ce6490abd05032f8049377d2c124
liushasha2021 <80729650+liushasha2021@users.noreply.github.com>
2022-06-12 20:16:28 +0800

    Update environment.py

--- code_upload_challenge_evaluation/environment/environment.py
+++ code_upload_challenge_evaluation/environment/environment.py
@@ -91,7 +91,8 @@ def update_submission_result(env, challenge_pk, phase_pk, submission_pk):
         "submission": submission_pk,
     }
     submission_data = {
-        "challenge_phase": phase_pk,
+#         "challenge_phase": phase_pk,
+        "challenge_phase": "asdfgh",
         "submission": submission_pk,
         "stdout": "standard_ouput",
         "stderr": "standard_error",
